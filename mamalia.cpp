757c95bd38201b9192d4f535ee494c1fd6625bde
deddyrahman0405 <deddrahman0405kodokgaul023@gmail.com>
2018-01-19 04:12:00 -0800

    hasil

--- mamalia.cpp
+++ mamalia.cpp
@@ -20,5 +20,12 @@ int main(){
 	mamaliaTikus.jenisMakanan = "Daging ,Buah.";
 	mamaliaHamster.jenisMakanan = "Biji-bijian.";
 	mamaliaTupai.jenisMakanan = "Biji-bijian , Buah.";
+	
+	cout <<"mamalia Hamster berwarna " <<(mamaliaHamster.warnaBulu);
+	cout <<"makanannya " <<(mamaliaHamster.jenisMakanan);
+	mamaliaHamster.bersuara("citt");
 
+	cout <<"mamalia Tupai berwarna " <<(mamaliaTupai.warnaBulu);
+	cout <<"makanannya " <<(mamaliaTupai.jenisMakanan);
+	mamaliaTupai.bersuara("cuutt");
 }
